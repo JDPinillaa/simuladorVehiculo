9bf83447a338e0a15ff1a0822a7f94e1e0def2c9
unknown <evers.uribeq@autonoma.edu.co>
2025-04-06 17:58:18 -0500

    correccion

--- simuladorDeVehiculo/src/autonoma/main/Main.java
+++ simuladorDeVehiculo/src/autonoma/main/Main.java
@@ -1,6 +1,5 @@
 package autonoma.main;
 
-
 import autonoma.models.*;
 import autonoma.exceptions.*;
 import autonoma.views.*;
@@ -12,22 +11,30 @@ public class Main {
         try {
             // Leer archivo y obtener configuración
             String[] configuracion = PersistenciaVehiculos.leerConfiguracionVehiculo();
-            String tipoLlantas = configuracion[0]; // Bonitas
-            String tipoMotor = configuracion[1];   // 3000
+            String tipoLlantas = configuracion[0]; // Ej: "Bonitas"
+            String tipoMotor = configuracion[1];   // Ej: "3000"
 
             // Instanciar motor
             Motor motor;
-            if (tipoMotor.equals("3000")) {
-                motor = new Motor3000();
-            } else if (tipoMotor.equals("1000")) {
-                motor = new Motor1000();
-            } else {
-                throw new IllegalArgumentException("Tipo de motor no válido: " + tipoMotor);
+            switch (tipoMotor) {
+                case "3000":
+                    motor = new Motor3000();
+                    break;
+                case "2000":
+                    motor = new Motor2000();
+                    break;
+                case "1000":
+                    motor = new Motor1000();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Tipo de motor no válido: " + tipoMotor);
             }
 
             // Instanciar llantas
             Llanta llantas;
             if (tipoLlantas.equalsIgnoreCase("Bonitas")) {
+                llantas = new LlantasBonitas();
+            } else if (tipoLlantas.equalsIgnoreCase("Buenas")) {
                 llantas = new LlantasBuenas();
             } else if (tipoLlantas.equalsIgnoreCase("Baratas")) {
                 llantas = new LlantasBaratas();
@@ -43,21 +50,17 @@ public class Main {
             carro.acelerar(50);
             carro.frenar(20);
             carro.mostrarEstado();
-            
-        } catch (CarroYaEncendidoException e) {
-             System.err.println("Error: " + e.getMessage());
-        } catch (CarroApagadoException e) {
-             System.err.println("Error: " + e.getMessage());
-        } catch (CarroAccidentadoException e) {
-             System.err.println("Error: " + e.getMessage());
-        } catch (CarroDetenidoException e) {
-             System.err.println("Error: " + e.getMessage());
-        } catch (CarroHaPatinadoException e) {
+
+        } catch (CarroYaEncendidoException | CarroApagadoException |
+                 CarroAccidentadoException | CarroDetenidoException |
+                 CarroHaPatinadoException e) {
+            System.err.println("Error: " + e.getMessage());
         } catch (Exception e) {
-             System.err.println("Error: " + e.getMessage());
+            System.err.println("Error general: " + e.getMessage());
         }
 
         // Mostrar cabina
         java.awt.EventQueue.invokeLater(() -> new Cabina().setVisible(true));
     }
 }
+
